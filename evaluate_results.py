7d56d02ae855338e8250308506bfde3693c2fe2d
PatWalters <wpwalters@gmail.com>
2018-09-03 15:58:19 -0400

    added solubility_comparison.py and evaluate_results.py

--- evaluate_results.py
+++ evaluate_results.py
@@ -23,12 +23,25 @@ def pearson_confidence(r, num, interval=0.95):
 
 
 def calc_pearson(pred, truth):
+    """
+    Calculate a Pearson R**2 + confidence intervals for a set of predicted and true values
+    :param pred: predicted values
+    :param truth: true (experimental values)
+    :return: Pearson R**2, lower and upper 95% confidence intervals
+    """
     pearson_r_val = pearsonr(truth, pred)[0]
     lower, upper = pearson_confidence(pearson_r_val, len(pred))
     return [x ** 2 for x in [pearson_r_val, lower, upper]]
 
 
 def eval_results(infile_name):
+    """
+    Read a csv file with solubility results
+    Columns are SMILES, Name, True values, all subsequent columns are predictions
+    Generates png files with R**2 and R**2 + confidence intervals
+    :param infile_name: input file name
+    :return: None
+    """
     df = pd.read_csv(infile_name)
     truth_col = df.columns[2]
     res = []
@@ -36,6 +49,7 @@ def eval_results(infile_name):
         r, lower, upper = calc_pearson(df[truth_col].values, df[col].values)
         res.append([col, r, lower, upper])
     df = pd.DataFrame(res, columns=["Method", "R**2", "Lower", "Upper"])
+    print(df)
     draw_histogram(df, "Solubility Comparison", "solubility_comparison.png", "Method", "R**2")
     draw_histogram(df, "Solubility Comparison", "solubility_comparison_error.png", "Method", "R**2", "Lower", "Upper")
 
